2038b6a8519b369321f770218e1380d9f755c4a9
Marco Hernandez <marco7245@hotmail.com>
2023-03-15 22:22:31 -0600

    MUI con Vite Añadido

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,7 +2,7 @@
 export const LoginPage = () => {
   return (
     <div>
-      
+        <h1>Login Page</h1>
     </div>
   )
 }
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@
 export const RegisterPage = () => {
   return (
     <div>
-      
+      <h1>Register Page</h1>
     </div>
   )
 }
