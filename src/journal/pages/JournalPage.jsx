4eeeac72f6125c3d6b7814e135ef09b0030839b3
Marco Hernandez <marco7245@hotmail.com>
2023-03-16 20:02:15 -0600

    View Añadido(Notas Vacias)

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
-import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views/NothingSelectedView'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Ad eiusmod esse sit ipsum reprehenderit veniam irure do dolore ex dolore.</Typography>
+      {/* <Typography>Ad eiusmod esse sit ipsum reprehenderit veniam irure do dolore ex dolore.</Typography> */}
+      <NothingSelectedView/>
     </JournalLayout>
   )
 }
