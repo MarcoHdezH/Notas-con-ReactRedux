80c47cbd9759bbb0282fb6e027baebeb4a70847d
Marco Hernandez <marco7245@hotmail.com>
2023-03-16 20:53:16 -0600

    Boton Flotante Añadido

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView } from '../views/NoteView'
 import { NothingSelectedView } from '../views/NothingSelectedView'
@@ -6,8 +8,22 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Ad eiusmod esse sit ipsum reprehenderit veniam irure do dolore ex dolore.</Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor:'error.main',
+          ':hover':{backgroundColor:'error.main',opacity:0.9},
+          position:'fixed',
+          right:50,
+          bottom:50
+          }}
+      >
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
+      {/* <NoteView/> */}
     </JournalLayout>
   )
 }
