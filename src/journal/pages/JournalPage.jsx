5f8dfb29038b9854306c0dff8a3972bdd7b3ed4d
Marco Hernandez <marco7245@hotmail.com>
2023-03-16 20:36:51 -0600

    Galeria de Imagenes Añadida

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView } from '../views/NoteView'
 import { NothingSelectedView } from '../views/NothingSelectedView'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Ad eiusmod esse sit ipsum reprehenderit veniam irure do dolore ex dolore.</Typography> */}
-      <NothingSelectedView/>
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
     </JournalLayout>
   )
 }
