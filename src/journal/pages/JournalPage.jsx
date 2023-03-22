29c8c2cf39db6bcae659d93b278a38f3d069b913
Marco Hernandez <marco7245@hotmail.com>
2023-03-22 12:32:10 -0600

    Creacion de Nuevas Notas en FireStore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,27 +1,35 @@
 import { AddOutlined } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { startNewNote } from '../../store/journal/thunks'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView } from '../views/NoteView'
 import { NothingSelectedView } from '../views/NothingSelectedView'
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
   return (
     <JournalLayout>
       {/* <Typography>Ad eiusmod esse sit ipsum reprehenderit veniam irure do dolore ex dolore.</Typography> */}
-      <NothingSelectedView/>
+      <NothingSelectedView />
 
       <IconButton
+        onClick={ onClickNewNote }
         size='large'
         sx={{
-          color:'white',
-          backgroundColor:'error.main',
-          ':hover':{backgroundColor:'error.main',opacity:0.9},
-          position:'fixed',
-          right:50,
-          bottom:50
-          }}
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
       >
-        <AddOutlined sx={{fontSize:30}}/>
+        <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
       {/* <NoteView/> */}
     </JournalLayout>
